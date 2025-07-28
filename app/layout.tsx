443afcd6c9a237fba0c6f3e4c8742e57e7aaeac8
RH0DYX <139616085+0XRH0DY@users.noreply.github.com>
2025-07-28 14:54:37 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,17 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
-export const metadata: Metadata = {
-  title: 'MOODENGS ON BONK',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+export const metadata = {
+  title: 'Moodengs on Bonk',
+  description: 'The biggest PFP cult on Bonk',
+  icons: {
+    icon: '/favicon-32x32.png',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
 }
+
 
 export default function RootLayout({
   children,
